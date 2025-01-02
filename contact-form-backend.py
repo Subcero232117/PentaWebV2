5c63bc811654d1bc8d6d799d432d8c7e6bdbb8dd
Subcero232117 <146759725+Subcero232117@users.noreply.github.com>
2025-01-01 21:44:51 -0400

    Update contact-form-backend.py

--- contact-form-backend.py
+++ contact-form-backend.py
@@ -11,7 +11,7 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'santosramonsteven@gmail.com'  # Tu correo
-app.config['MAIL_PASSWORD'] = 'tu_contraseña_de_aplicacion'  # Contraseña de aplicación de Gmail
+app.config['MAIL_PASSWORD'] = '123455'  # Contraseña de aplicación de Gmail
 
 mail = Mail(app)
 
